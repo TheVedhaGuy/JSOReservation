c3665fdcb6eb6edbdfcfa84bc98dab1e0b92b5ae
TheVedhaGuy <vedha.rajasegaran@gmail.com>
2018-02-11 01:30:43 -0500

    Handle production keys and dev keys

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+//dev.js Don't commit this
+module.exports = {
+    googleClientID: '918556981919-gumo458qunv561ffof9t9k7icjed0ukq.apps.googleusercontent.com',
+    googleClientSecret: '7VaBSJNImDeVVU8GgFuhs4NY',
+    dbHost: '107.180.12.18',
+    dbUser: 'jetskireserve',
+    dbPass: 'Jetski2018!',
+    mongoURI: 'mongodb://jetskireserve:Jetski2018!@ds149613.mlab.com:49613/jsoreserervations',
+    cookieKey: 'asdasd809asd0as8d0a9s8d0asdja'
+};
